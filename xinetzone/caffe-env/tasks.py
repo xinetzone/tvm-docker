734cc57135c7c4b015311d15e53f8b124f0e3ea8
liuxinwei <liuxinwei@xmsilicon.cn>
2024-03-14 15:01:41 +0800

    修改：     xinetzone/caffe-env/tasks.py

--- xinetzone/caffe-env/tasks.py
+++ xinetzone/caffe-env/tasks.py
@@ -13,17 +13,17 @@ def tar(ctx):
     ctx.run(cmd)
 
 @task
-def compile(ctx):
+def compile(ctx, root="/data"):
     # 编译 bootst
-    cmd = "cd /data/boost-1.81.0/ && ./bootstrap.sh"
+    cmd = f"cd {root}/boost-1.81.0/ && ./bootstrap.sh"
     cmd += "&& ./b2 cflags='-fPIC' cxxflags='-fPIC' ./b2 install"
     # 编译 protobuf
-    cmd += "&& cd /data/protobuf-3.17.3/ && ./autogen.sh"
+    cmd += f"&& cd {root}/protobuf-3.17.3/ && ./autogen.sh"
     cmd += "&& ./configure && make -j$(nproc)"
     cmd += "&& make check -j$(nproc)"
     cmd += "&& make install && ldconfig"
     # 编译 caffe 
-    cmd += "&& cd /data/caffe && make all -j$(nproc) && make pycaffe -j$(nproc)"
+    cmd += f"&& cd {root}/caffe && make all -j$(nproc) && make pycaffe -j$(nproc)"
     # 清理
-    cmd += "&& rm -rf /data/boost-1.81.0/ && rm -rf /data/protobuf-3.17.3/"
+    cmd += f"&& rm -rf {root}/boost-1.81.0/ && rm -rf {root}/protobuf-3.17.3/"
     ctx.run(cmd)
