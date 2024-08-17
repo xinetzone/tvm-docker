174c25c089485d7fe230d859ec74b7818c89be7c
liuxinwei <liuxinwei@xmsilicon.cn>
2024-08-17 13:55:12 +0800

    同上

--- xinetzone/caffe-env/tasks.py
+++ xinetzone/caffe-env/tasks.py
@@ -3,8 +3,8 @@ from invoke import task
 @task
 def tar(ctx):
     # 清除遗留文件
-    cmd = "rm -rf temp"
-    # # 解压文件
+    cmd = "rm -rf temp&&mkdir -p temp"
+    # 解压文件
     cmd += "&& tar -xvf caffe.tar.gz && tar -xvf boost-1.81.0.tar.gz" 
     cmd += "&& tar -xvf protobuf-3.17.3.tar.gz "
     cmd += "&& mkdir -p temp && mv protobuf-3.17.3 temp/protobuf-3.17.3"
